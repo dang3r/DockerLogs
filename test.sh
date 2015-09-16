34ac5632b0926bd4bf64ffa8717a399b94c104ad
Daniel Cardoza <danielpcardoza@gmail.com>
2015-09-16 19:31:25 -0400

    [testing] Fixed checking if container no longer up

--- test.sh
+++ test.sh
@@ -8,9 +8,11 @@ track_stats() {
 	
 	DATE=$(date +%Y-%m-%d_%H_%M_%S)
 	STATS_FILE=$1_stats_$DATE.txt
+	touch error.txt
 	while true; do
-		docker stats --no-stream=true $1 >> $STATS_FILE 2>&1
-		if [ $? -ne 0 ]; then
+		docker stats --no-stream=true $1 >> $STATS_FILE 2>> error.txt
+		ERROR=$(wc -l < error.txt)
+		if [ $ERROR -ne 0 ]; then
 			exit 0
 		fi
 		sleep 0.5
