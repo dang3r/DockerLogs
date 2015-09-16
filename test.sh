7e39bacba280d67243abd2ef10625d10819c7634
Daniel Cardoza <danielpcardoza@gmail.com>
2015-09-16 11:59:20 -0400

    [testing] Improve test
    - docker container stats are now naively monitored. Need to fix
      a parsing error with IO redirection
    - we let all messages retrieve the log now. A better way might be to
      monitor connections on certain ports, instead of checking messages.

--- test.sh
+++ test.sh
@@ -1,32 +1,77 @@
-#!/bin/bash -ex
+#!/bin/bash 
+
+## init ##
+STOCK_IMAGE="ubuntu"
+SIZE=1000000
+
+track_stats() {
+	
+	DATE=$(date +%Y-%m-%d_%H_%M_%S)
+	STATS_FILE=$1_stats_$DATE.txt
+	while true; do
+		docker stats --no-stream=true $1 >> $STATS_FILE 2>&1
+		if [ $? -ne 0 ]; then
+			exit 0
+		fi
+		sleep 0.5
+	done
+}
 
 # Retrieve container IP given ID
 get_container_ip() {
 	docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
 }
 
-for IMAGE in rsyslog logstash; do
+for IMAGE in rsyslog; do
+	
+	echo "Running $IMAGE container and initializating stat tracking"
 	CID=$(docker run -d --name $IMAGE --privileged $IMAGE)
 	CIP=$(get_container_ip $CID)
+	track_stats $IMAGE &
 	sleep 5
+	
+	echo "Initializing tenant containers."
 	for i in {0..2}; do
-		NAME="test_$i"
-		SIZE=1000000
+		NAME="test_$IMAGE_$i"
 		docker run \
+			--detach=true \
 			--name $NAME \
+			--env CLIENT=$i \
 			--privileged \
 			--log-driver=syslog \
 			--log-opt syslog-address=tcp://$CIP:1338 \
-			ubuntu \
-			bash -c 'for i in {0..1000000}; do echo "3BWTR_Message$i"; done'
-		docker rm -f $NAME
+			$STOCK_IMAGE \
+			bash -c 'for i in {0..1000000}; do echo "Client$CLIENT_$i"; done'
 	done
+
+	echo "Waiting for tenant containers to die."
+	while true; do
+			if [ $(docker ps -q | wc -l) -eq 1 ]; then
+					break
+			fi
+			sleep 5
+	done
+
+	sleep 10
 	echo "$IMAGE : expected ~3000000"
+	BASE="0"
 	if [ "$IMAGE" == "rsyslog" ]; then
-		docker exec -ti $CID wc -l /var/log/syslog
+		while true; do
+				NEW=$(docker exec $CID wc -l /var/log/syslog | awk {'print $1 '})
+				echo "NEW=$NEW BASE=$BASE"
+				if [ "$NEW" == "$BASE" ]; then
+					echo "RETRIEVED $NEW"
+					break
+				fi
+				BASE=$NEW
+		done
 	elif [ "$IMAGE" == "logstash" ]; then
-		docker exec -ti $CID wc -l /var/log/logstash_ingest.log
+		docker exec $CID wc -l  < /var/log/logstash_ingest.log
 	fi
-	
+
 	read -p "Press any key to continue.."
+	echo "Removing $IMAGE container."
+	docker rm -f $IMAGE
+	docker rm -f $(docker ps -aq)
+
 done
